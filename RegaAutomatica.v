4ba14491d3cdbf3e9faf7321730b2583030ea2bb
Cleidson Ramos <cleidson.ramos788@gmail.com>
2024-04-24 20:22:23 -0300

    Update RegaAutomatica.v

--- RegaAutomatica.v
+++ RegaAutomatica.v
@@ -1,13 +1,13 @@
 // Declaracao do modulo
 module RegaAutomatica (H, M, L, Us, Ua, T, Sd, Ve, Al,
 							  Bs, Vs, SEG_D1, SEG_D2, SEG_D3, SEG_D4, SEG_A, SEG_B, 
-							  SEG_C, SEG_D, SEG_E, SEG_F, SEG_G);	
+							  SEG_C, SEG_D, SEG_E, SEG_F, SEG_G, SEG_P, LED_R, LED_G, LED_B);	
 							  
 	// Declaracao de portas
 	input H, M, L, Us, Ua, T, Sd;
 	output Bs, Vs, Ve, Al; 
 	output SEG_D1, SEG_D2, SEG_D3, SEG_D4;
-	output SEG_A, SEG_B, SEG_C, SEG_D, SEG_E, SEG_F, SEG_G;
+	output SEG_A, SEG_B, SEG_C, SEG_D, SEG_E, SEG_F, SEG_G, SEG_P, LED_R, LED_G, LED_B;
 	
 	// Declaracao dos fios intermediarios
 	wire Nv_Critico, Nv_Baixo, Nv_Medio, Nv_Alto, ERRO;
@@ -21,7 +21,10 @@ module RegaAutomatica (H, M, L, Us, Ua, T, Sd, Ve, Al,
 	Irrigacao Irr1 (Us, Ua, T, M, L, ERRO, Bs, Vs);
 	
 	//Modulo de Controle do Mostrador de & segmentos
-	Mostrador7 Most1 (Nv_Critico, Nv_Baixo, Nv_Medio, Nv_Alto, ERRO, Bs, Vs, Sd,  
-	SEG_D1, SEG_D2, SEG_D3, SEG_D4, SEG_A, SEG_B, SEG_C, SEG_D, SEG_E, SEG_F, SEG_G);
+	Mostrador7 Most1 (Nv_Critico, Nv_Baixo, Nv_Medio, Nv_Alto, ERRO, Bs, Vs, Sd,
+							SEG_D1, SEG_D2, SEG_D3, SEG_D4, SEG_A, SEG_B, SEG_C, SEG_D, SEG_E, SEG_F, SEG_G, SEG_P);
 	
+	or ledR (LED_R, ERRO, Nv_Critico);
+	or ledG (LED_G, Nv_Critico, Nv_Alto, Nv_Baixo);
+	or ledB (LED_B, Nv_Baixo, Nv_Medio);
 endmodule 
