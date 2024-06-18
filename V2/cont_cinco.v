52adc4d55bd6f7da878f61441ffccb6832fcda21
Pedro Cruz <60475001+pedroc07@users.noreply.github.com>
2024-06-18 13:50:20 -0300

    Update cont_cinco.v

--- V2/cont_cinco.v
+++ V2/cont_cinco.v
@@ -1,13 +1,13 @@
 //Definição do módulo Contador decrescente de 5 a 0
 
-module cont_cinco(Clk, Rst, Q2, Q1, Q0);
+module cont_cinco(Clk, Pos0, Pos5, Q2, Q1, Q0);
 
-input Clk, Rst;
+input Clk, Pos0, Pos5;
 
 output Q2, Q1, Q0;
 
 //Fios intermediários
-wire Wire1, Wire2;
+wire Wire1, Wire2, Wire3;
 wire Q2n, Q1n, Q0n;
 
 not Not0(Q2n, Q2);
@@ -16,13 +16,14 @@ not Not2(Q0n, Q0);
 
 and And0(Wire1, Q2, Q0n);
 and And1(Wire2, Q1n, Q0n);
+or Or0(Wire3, Pos0, Pos5);
 
 //Flip-flops JK usados para fazer o contador
 //FF0
-flipflopJK(1, 1, Rst, 0, Clk, Q0);
+flipflopJK(1, 1, Pos0, Pos5, Clk, Q0);
 //FF1
-flipflopJK(Wire1, Q0n, Rst, 0, Clk, Q1);
+flipflopJK(Wire1, Q0n, Wire3, 0, Clk, Q1);
 //FF2
-flipflopJK(Wire2, Q0n, Rst, 0, Clk, Q2);
+flipflopJK(Wire2, Q0n, Pos0, Pos5, Clk, Q2);
 
 endmodule
